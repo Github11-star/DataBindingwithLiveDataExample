77ebf46721d2ec20d876f62d640c30047df3d249
Firoz Shaikh <gitfiroz695@gmail.com>
2023-08-14 14:25:02 +0530

    Data Binding with LiveData

---
+++ app/src/main/java/com/firozpocyt/databindingwithlivedata/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.firozpocyt.databindingwithlivedata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.firozpocyt.databindingwithlivedata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var mainViewModel: MainViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        binding.mainViewModel = mainViewModel
+        binding.lifecycleOwner = this
+    }
+}
